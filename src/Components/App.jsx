3b23a228d4a69cf3f0d5827a17c53fc45b7907d1
JPTrey <jonpaul.simonelli@gmail.com>
2024-02-02 20:56:26 -0500

    Add Redux functionality for Cart behavior.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,77 +1,85 @@
-import { createContext, useEffect, useState } from 'react'
-import Router from './Router.jsx'
-import { defaultCart } from './DefaultCart.jsx'
-import '../index.css'
+import { createContext, useEffect, useState } from "react";
+import Router from "./Router.jsx";
+import { defaultCart } from "./DefaultCart.jsx";
+import "../index.css";
 
 const StoreContext = createContext({
   cartItems: [],
   shopItems: [],
   updateCart: () => {},
-})
+});
 
-const loadedCart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : defaultCart
+const loadedCart = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : defaultCart;
 
 function App() {
-  const [CartItems, setCartItems] = useState(loadedCart)
-  const [ShopItems, setShopItems] = useState([])
-  const [FetchFailed, setFetchFailed] = useState(false)
-  const [ErrorMessage, setErrorMessage] = useState('')
-  const [Loading, setLoading] = useState(true)
- 
+  const [CartItems, setCartItems] = useState(loadedCart);
+  const [ShopItems, setShopItems] = useState([]);
+  const [FetchFailed, setFetchFailed] = useState(false);
+  const [ErrorMessage, setErrorMessage] = useState("");
+  const [Loading, setLoading] = useState(true);
+
   function updateCart(cart) {
-    setCartItems(cart)
-    localStorage.setItem('cart', JSON.stringify(cart))
+    setCartItems(cart);
+    localStorage.setItem("cart", JSON.stringify(cart));
   }
 
   async function fetchItems() {
     try {
-      await fetch('https://dummyjson.com/products', {mode: 'cors'})
-      .then(result=>result.json())
-      .then(json=>setShopItems(json.products))
-      .finally(setLoading(false))
-    }
-    catch (err) {
-      setErrorMessage(`${err}`)
-      setFetchFailed(true)
+      await fetch("https://dummyjson.com/products", { mode: "cors" })
+        .then((result) => result.json())
+        .then((json) => setShopItems(json.products))
+        .finally(setLoading(false));
+    } catch (err) {
+      setErrorMessage(`${err}`);
+      setFetchFailed(true);
     }
   }
 
   useEffect(() => {
-    fetchItems()
-  }, [])
+    fetchItems();
+  }, []);
 
   return (
     <>
-      {FetchFailed &&  <>
-            <div id="modal">
-                <div id="modal-message">
-                    <p>{`Looks like we failed to get the data necessary here!`}</p>
-                    <p>{`(${ErrorMessage})`}</p>
-                    <button onClick={() => location.reload()}>Refresh</button>
-                </div>
+      {FetchFailed && (
+        <>
+          <div id="modal">
+            <div id="modal-message">
+              <p>{`Looks like we failed to get the data necessary here!`}</p>
+              <p>{`(${ErrorMessage})`}</p>
+              <button onClick={() => location.reload()}>Refresh</button>
             </div>
-      </>}
-      {!FetchFailed && Loading && <>
-            <div id="modal">
-                <div id="modal-message">
-                    <p>{`Loading...`}</p>
-                </div>
+          </div>
+        </>
+      )}
+      {!FetchFailed && Loading && (
+        <>
+          <div id="modal">
+            <div id="modal-message">
+              <p>{`Loading...`}</p>
             </div>
-      </>}
-      {!FetchFailed && !Loading &&
-      <StoreContext.Provider value = {{
-        cartItems: CartItems,
-        shopItems: ShopItems,
-        updateCart: updateCart,
-      }}>
-        <Router />
-      </StoreContext.Provider>}
+          </div>
+        </>
+      )}
+      {!FetchFailed && !Loading && (
+        <StoreContext.Provider
+          value={{
+            cartItems: CartItems,
+            shopItems: ShopItems,
+            updateCart: updateCart,
+          }}
+        >
+          <Router />
+        </StoreContext.Provider>
+      )}
     </>
-  )
+  );
 }
 
-export default App
-export {StoreContext}
+export default App;
+export { StoreContext };
 
 // Do the fetch
 // Create & Provide the context
---
+++ src/features/cartSlice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { defaultCart } from "../Components/DefaultCart";
+
+const initialState = {
+  cartItems: defaultCart,
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      const item = action.payload;
+      state.cartItems = [...state.cartItems, item];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    updateQuantity: (state, action) => {
+      const { itemId, quantity } = action.payload;
+      state.cartItems = state.cartItems.map((item) =>
+        item.id === itemId ? { ...item, quantity } : item
+      );
+    },
+    incrementQuantity: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity + 1 } : item
+      );
+    },
+    decrementQuantity: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems.map((item) =>
+        item.id === itemId ? { ...item, quantity: item.quantity - 1 } : item
+      );
+    },
+  },
+});
+
+export const {
+  addItem,
+  removeItem,
+  incrementQuantity,
+  updateQuantity,
+  decrementQuantity,
+} = cartSlice.actions;
+export default cartSlice.reducer;
