cfc82dc4aa1335bc6c5a042e612ba965c43d5203
JPTrey <jptrey91@gmail.com>
2024-02-03 15:42:09 -0500

    Break Store, Category, and ShopItem into discrete components.  Redux is now used to managed state across all three.

---
+++ src/Components/Category/Category.jsx
@@ -0,0 +1,19 @@
+import { capitalize } from "../../functions";
+import ShopItem from "../ShopItem/ShopItem";
+
+const Category = ({ categoryName, categoryItems }) => {
+  return (
+    <div className="store-section-container" key={`${categoryName}`}>
+      <div className="store-section-header" id={categoryName}>{`${capitalize(
+        categoryName
+      )}`}</div>
+      <div className="store-section">
+        {categoryItems.map((item) => (
+          <ShopItem item={item} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
