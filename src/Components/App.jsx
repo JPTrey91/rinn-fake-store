1fbca6272094133986c21e938852937f13d9cd14
JPTrey <jptrey91@gmail.com>
2024-02-09 14:13:29 -0500

    Integrate RTKQuery for Product fetching; remove functionality from shopSlice and App component.

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,58 +1,8 @@
-import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
-import { stockItems } from "../features/shopSlice.js";
 import "../index.css";
 import Router from "./Router.jsx";
 
 function App() {
-  const [FetchFailed, setFetchFailed] = useState(false);
-  const [ErrorMessage, setErrorMessage] = useState("");
-  const [Loading, setLoading] = useState(true);
-
-  const dispatch = useDispatch();
-
-  function fetchItems() {
-    try {
-      fetch("https://dummyjson.com/products", { mode: "cors" })
-        .then((result) => result.json())
-        .then((json) => dispatch(stockItems(json.products)))
-        .finally(setLoading(false));
-    } catch (err) {
-      setErrorMessage(`${err}`);
-      setFetchFailed(true);
-    }
-  }
-
-  useEffect(() => {
-    fetchItems();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return (
-    <>
-      {FetchFailed && (
-        <>
-          <div id="modal">
-            <div id="modal-message">
-              <p>{`Looks like we failed to get the data necessary here!`}</p>
-              <p>{`(${ErrorMessage})`}</p>
-              <button onClick={() => location.reload()}>Refresh</button>
-            </div>
-          </div>
-        </>
-      )}
-      {!FetchFailed && Loading && (
-        <>
-          <div id="modal">
-            <div id="modal-message">
-              <p>{`Loading...`}</p>
-            </div>
-          </div>
-        </>
-      )}
-      {!FetchFailed && !Loading && <Router />}
-    </>
-  );
+  return <Router />;
 }
 
 export default App;
--- src/Components/Store/Store.jsx
+++ src/Components/Store/Store.jsx
@@ -11,81 +11,87 @@ import Category from "../Category/Category.jsx";
 import Header from "../Header/Header.jsx";
 import "./Store.css";
 import { MAX_QUANTITY } from "../../constants.js";
+import { useFetchProductsQuery } from "../Api/productsApi.js";
 
 function Store() {
   if (!window.location.href.includes("store")) window.scrollTo(0, 0);
   const isShowingSidebar = useSelector((state) => state.shop.isShowingSidebar);
   const isShowingModal = useSelector((state) => state.shop.isShowingModal);
-  const shopItems = useSelector((state) => state.shop.shopItems);
   const dispatch = useDispatch();
 
-  let categories = [];
-  shopItems.forEach((item) => {
-    if (!categories.includes(item.category)) {
-      categories.push(item.category);
-    }
-  });
+  const { data, isLoading, error } = useFetchProductsQuery();
 
-  return (
-    <>
-      <Header />
-      {isShowingModal && (
-        <>
-          <div id="modal">
-            <div id="modal-message">
-              <p>
-                Uh oh, you can't add more than {MAX_QUANTITY} of the same item
-                to your cart! It's just a matter of our limited stock.
-              </p>
-              <button onClick={() => dispatch(hideModal())}>OK</button>
+  if (isLoading) return <div>Loading Products...</div>;
+  else if (error) return <div>Error loading products...</div>;
+  else {
+    const { products: shopItems } = data;
+    let categories = [];
+    shopItems.forEach((item) => {
+      if (!categories.includes(item.category)) {
+        categories.push(item.category);
+      }
+    });
+    return (
+      <>
+        <Header />
+        {isShowingModal && (
+          <>
+            <div id="modal">
+              <div id="modal-message">
+                <p>
+                  Uh oh, you can't add more than {MAX_QUANTITY} of the same item
+                  to your cart! It's just a matter of our limited stock.
+                </p>
+                <button onClick={() => dispatch(hideModal())}>OK</button>
+              </div>
             </div>
-          </div>
-        </>
-      )}
-      <div id="store-container">
-        {!isShowingSidebar && (
-          <img
-            id="store-sidebar-open"
-            src={RightArrow}
-            onClick={() => dispatch(showSidebar())}
-          ></img>
+          </>
         )}
-        {isShowingSidebar && (
-          <div id="store-sidebar">
-            <div id="store-sidebar-content">
-              <img
-                id="store-sidebar-close"
-                src={LeftArrow}
-                onClick={() => dispatch(hideSidebar())}
-              ></img>
-              <h2>Categories</h2>
-              {categories.map((category) => {
-                return (
-                  <h3 key={category}>
-                    <a href={`#${category}`}>{`${capitalize(category)}`}</a>
-                  </h3>
-                );
-              })}
+        <div id="store-container">
+          {!isShowingSidebar && (
+            <img
+              id="store-sidebar-open"
+              src={RightArrow}
+              onClick={() => dispatch(showSidebar())}
+            ></img>
+          )}
+          {isShowingSidebar && (
+            <div id="store-sidebar">
+              <div id="store-sidebar-content">
+                <img
+                  id="store-sidebar-close"
+                  src={LeftArrow}
+                  onClick={() => dispatch(hideSidebar())}
+                ></img>
+                <h2>Categories</h2>
+                {categories.map((category) => {
+                  return (
+                    <h3 key={category}>
+                      <a href={`#${category}`}>{`${capitalize(category)}`}</a>
+                    </h3>
+                  );
+                })}
+              </div>
             </div>
+          )}
+          <div id="store-main">
+            {categories.map((category) => {
+              const categoryItems = shopItems.filter(
+                (item) => item.category === category
+              );
+              return (
+                <Category
+                  key={category}
+                  categoryName={category}
+                  categoryItems={categoryItems}
+                />
+              );
+            })}
           </div>
-        )}
-        <div id="store-main">
-          {categories.map((category) => {
-            const categoryItems = shopItems.filter(
-              (item) => item.category === category
-            );
-            return (
-              <Category
-                key={category}
-                categoryName={category}
-                categoryItems={categoryItems}
-              />
-            );
-          })}
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Store;
